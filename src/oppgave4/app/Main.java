bf13a1eb472c51470795ea5ed14020a951266b06
NimaAB <nimabewrani98@gmail.com>
2020-03-21 20:47:52 +0100

    Feil fiksed i oppgave4.app.Main.java

--- src/oppgave4/app/Main.java
+++ src/oppgave4/app/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("app/view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         primaryStage.setTitle("HTML Editor");
         primaryStage.setScene(new Scene(root, primaryStage.getMinWidth(), primaryStage.getMinHeight()));
         primaryStage.show();
